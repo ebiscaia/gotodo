531dabb5597cb4f154e48c959a80b2900b6719de
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-08-06 21:08:29 +1000

    Create a slice called users and filled with data

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ func main() {
 	todos = append(todos, Todo{name: "take the puppy for a lap", user: "eddie"})
 	todos = append(todos, Todo{name: "take the rubbish out", user: "eddie"})
 
+	//mockup list of todos
+	users := []User{}
+	users = append(users, User{name: "user1", pass: "pass1"}, User{name: "user2", pass: "pass2"})
+
 	//loop through the todo list
 	for _, todo := range todos {
 		fmt.Printf("Todo: %v\n", todo.name)
