e073c2d990a7e07236c62d9356aca18df1c34716
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-09-22 17:52:53 +1000

    chore: increased time of context.Timeout

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 	json.Unmarshal(byteValue, &mongoCredentials)
 
 	//create the context and connect to the server using the credentials
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(options.Client().ApplyURI(mongoCredentials.MongoURI()))
 	if err != nil {
