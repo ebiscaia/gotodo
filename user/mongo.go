d61457c7baa09e2be1062605ab123fd75830a845
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-09-26 19:31:53 +1000

    fix: removed ctx argument from the function

--- user/mongo.go
+++ user/mongo.go
@@ -2,13 +2,16 @@ package user
 
 import (
 	"context"
+	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 // check whether the collection has at least one document
-func CheckUsers(ctx context.Context, col *mongo.Collection) (bool, error) {
+func CheckUsers(col *mongo.Collection) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 	hasUsers, err := col.CountDocuments(ctx, bson.M{})
 	if err != nil {
 		return false, err
@@ -19,7 +22,10 @@ func CheckUsers(ctx context.Context, col *mongo.Collection) (bool, error) {
 	return true, nil
 }
 
-func FindUserDB(ctx context.Context, col *mongo.Collection, usr User) (User, error) {
+func FindUserDB(col *mongo.Collection, usr User) (User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	userToFind := User{}
 	// This function is for login purposes. Therefore, user name is to be a exact match
 	err := col.FindOne(ctx,
@@ -30,7 +36,10 @@ func FindUserDB(ctx context.Context, col *mongo.Collection, usr User) (User, err
 	return userToFind, nil
 }
 
-func AddUserToDB(ctx context.Context, col *mongo.Collection, us User) error {
+func AddUserToDB(col *mongo.Collection, us User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	_, err := col.InsertOne(ctx, us)
 	if err != nil {
 		return err
