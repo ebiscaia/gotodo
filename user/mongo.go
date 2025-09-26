0157f8036a4f49fb2b0b7aa4550b42df3b8bce52
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-09-26 18:55:07 +1000

    feature: created function findUserDB

--- user/mongo.go
+++ user/mongo.go
@@ -19,6 +19,17 @@ func CheckUsers(ctx context.Context, col *mongo.Collection) (bool, error) {
 	return true, nil
 }
 
+func FindUserDB(ctx context.Context, col *mongo.Collection, usr User) (User, error) {
+	userToFind := User{}
+	// This function is for login purposes. Therefore, user name is to be a exact match
+	err := col.FindOne(ctx,
+		bson.M{"name": usr.Name}).Decode(&userToFind)
+	if err != nil {
+		return User{}, err
+	}
+	return userToFind, nil
+}
+
 func AddUserToDB(ctx context.Context, col *mongo.Collection, us User) error {
 	_, err := col.InsertOne(ctx, us)
 	if err != nil {
