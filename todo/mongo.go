cdb671aeff04e7019bc151fe06cefbd0e824e1bd
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-10-10 17:57:13 +1100

    feature: defined function ChangeStatus

--- todo/mongo.go
+++ todo/mongo.go
@@ -25,6 +25,22 @@ func RemoveTodoAtIndex(col *mongo.Collection, usrTodos []bson.ObjectID, index in
 	return nil
 }
 
+func ChangeStatus(col *mongo.Collection, usrTodos []bson.ObjectID, index int, status bool) error {
+	// retrieve todo based on its id
+	println(status)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// _, err := col.DeleteOne(ctx, bson.M{"_id": usrTodos[index]})
+	_, err := col.UpdateOne(ctx, bson.M{"_id": usrTodos[index]}, bson.D{{"$set", bson.D{{"isDone", status}}}})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func GetTodo(col *mongo.Collection, usrTodos []bson.ObjectID, index int) (Todo, error) {
 	// retrieve todo based on its id
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
