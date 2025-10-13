4507928d7f7777a0467dcf95a2786fcfc686fcf0
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-10-13 17:52:22 +1100

    refactor: moved lines out of else block

--- todo/todo.go
+++ todo/todo.go
@@ -60,9 +60,9 @@ func CreateTodo(col *mongo.Collection, usrLogin user.User) {
 			fmt.Printf("%v\n", err)
 		}
 	} else {
-		fmt.Println("There was an error with todo creation. Leaving...")
-		os.Exit(1)
 	}
+	fmt.Println("There was an error with todo creation. Leaving...")
+	os.Exit(1)
 }
 
 func inputIndex(lenTodo int, funcParent string) int {
